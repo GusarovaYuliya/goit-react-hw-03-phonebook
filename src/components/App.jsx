9704b0a71568522c253c5e89f32bbf774cc563a4
GusarovaYuliya <gusarovayuliya25@gmail.com>
2023-10-27 01:31:56 +0300

    add.localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,6 +38,21 @@ class App extends Component{
     this.setState({ filter: e.currentTarget.value });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     return (
       <div className={css.appContainer}>

9cf98d6ebaaa3f9823f811eaf3361f6fb8b9e783
GusarovaYuliya <gusarovayuliya25@gmail.com>
2023-11-01 00:24:34 +0300

    comm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,15 @@ import Search from "./Search/Search";
 import ContactList from "./ContactList/ContactList";
 import css from 'App.module.css'
 
-class App extends Component{
+class App extends Component {
 
   state = {
     contacts: [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
 
     filter: '',
   };
@@ -47,42 +48,13 @@ class App extends Component{
     }
   }
 
-  // addContact = contact => {
-  //   const isInContacts = this.state.contacts.some(
-  //     ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
-  //   );
-  //   if (isInContacts) {
-  //     alert(`${contact.name} is already in contacts`);
-  //     return;
-  //   };
-  //   this.setState(prevState => ({
-  //     contacts: [...prevState.contacts, contact],
-  //   }));
-  // }
-  // changeFilter = value => {
-  //   this.setState({ filter: value });
-  // };
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
-  // getFilterContacts = () => {
-  //   const { filter, contacts } = this.state;
-  //   const normalizedFilter = filter.toLowerCase();
-
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
-  // componentDidUpdate(_, prevState) {
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
-  // deleteContact = contactId => {
-  //   this.setState(prevState => {
-  //     return {
-  //       contacts: prevState.contacts.filter(({ id }) => id !== contactId),
-  //     };
-  //   });
-  // };
+ 
   render() {
    
     return (
@@ -92,7 +64,8 @@ class App extends Component{
           createUser={this.createUser}
           userNumber={this.state.number}
           userName={this.state.name}
-          contacts={this.state.contacts} />
+          contacts={this.state.contacts}
+        />
          <p className={css.searchText}>Find contacts by name</p>
         <Search onChange={this.handlerSearch} value={this.state.filter} />
         <ContactList
